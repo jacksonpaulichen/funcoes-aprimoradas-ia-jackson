eea5f4685620fb9810e64a1083f9a3596da3bbf4
jackson paulichen <108669034+jacksonpaulichen@users.noreply.github.com>
2024-09-27 10:49:31 -0300

    Add files via upload

---
+++ js/aleatorio.js
@@ -0,0 +1,4 @@
+export function aleatorio(lista){
+    const posicao = Math0.floor(Math.random()*lista.length );
+    return lista[posicao]; 
+}
---
+++ js/perguntas.js
@@ -0,0 +1,79 @@
+export const perguntas = [
+    {
+        enunciado: "Qual tipo de carro você prefere ?",
+        alternativas: [
+            {
+                texto: "Um carro mais antigo",
+                afirmacao: [ 
+                "Você tem um estilo mais retrô gosta de carros mais conservados e originas",
+                "afirmação 2"
+                ]
+            },
+            {
+                texto: "Um carro mais novo",
+                afirmacao: [
+                "Você tem um estilo mais moderno, gosta de carros mais novos e modificados ",
+                "afirmação 2" 
+                ]
+            }
+        ]
+    },
+    { 
+        enunciado: "Qual estilo você mais gosta em um carro ? ",
+        alternativas: [
+            {
+                texto: "Carros mais altos ",
+                afirmacao: [
+                " Você prefere andar mais tranquilo sem correr riscos de enroscar o carro ou ficar preso em algum local ",
+                "afirmação 2" ]
+            },
+            {
+                texto: " Carros mais baixos",
+                afirmacao: [
+                "Você gosta de estetica porem se preucupa muito com a manutenção por conta do carro ser mais baixo e correr mais riscos ",
+                "afirmação 2"]
+            }
+        ]
+    },
+    {
+        enunciado: "Quais carros você prefere ?",
+        alternativas: [
+            {
+                texto: "Gol, Saveiro, Parati, Corsa, Celta, Palio",
+                afirmacao: [
+                "Você prefere carros mais economicos e prefere andar sem chamar muito a atenção",
+                "afirmação 2"]
+            },
+            {
+                texto: "Porche, Ferrari, pagani, bugatti, masserati, rolls royse",
+                afirmacao: "Você preferre carros mais esportivos, gosta de adrenalina e curte chamar a atenção ao passar pelos lugares",
+            }
+        ]
+    },
+    {
+        enunciado: "Para ir a uma corrida qual carro você escolheria ?",
+        alternativas: [
+            {
+                texto: "Porche, Ferrari, Lamborguine",
+                afirmacao: "Você gosta de andar em altas velocidades chamando a atenção por aonde passa",
+            },
+            {
+                texto: "Koenigsegg Jesko Absolut, Hennessey Venon, Astron Martin Valkyrie",
+                afirmacao: "Caramba você realmente gosta de altas velocidades e adrenalina em altas velocidades",
+            }
+        ]
+    },
+    {
+        enunciado: " Você praticaria rally ? ",
+        alternativas: [ 
+            {
+                texto: " Sim, Gosto muito ",
+                afirmacao: " Seu estilo faz com que você goste mais de aventuras e emoções ",
+            },
+            {
+                texto: " Não, acho perigoso ",
+                afirmacao: " Seu estilo faz com que você goste mais de segurança sem se arriscar muito, uma aventura mais segura ",
+            } 
+        ]
+    },
+];
